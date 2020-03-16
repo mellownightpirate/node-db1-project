b2beae042eb58b06a5d2422226dfe42074c932ce
Amin Hasan <amin@Amins-MacBook-Air.local>
2020-03-16 12:50:27 +0000

    completed post, put and delte api routers and functions

--- data/helpers/account-models.js
+++ data/helpers/account-models.js
@@ -10,7 +10,26 @@ function getAccountById(id) {
     .first();
 }
 
+function insertNewAccount({ name, budget }) {
+    return db("accounts").insert({ name, budget });
+  }
+  
+  function updateAccountById(id, { name, budget }) {
+    return db("accounts")
+      .where({ id: id })
+      .update({ name, budget });
+  }
+  
+  function deleteAccountById(id) {
+    return db("accounts")
+      .where({ id: id })
+      .delete();
+  }
+
 module.exports = {
   getAllAccounts,
-  getAccountById
+  getAccountById,
+  insertNewAccount,
+  updateAccountById,
+  deleteAccountById
 };
--- data/routers/account-routers.js
+++ data/routers/account-routers.js
@@ -25,4 +25,37 @@ router.get("/api/accounts/:id", (req, res) => {
     });
 });
 
+router.post("/api/accounts", (req, res) => {
+    accounts
+      .insertNewAccount(req.body)
+      .then(id => {
+        res.status(201).json(`Account created with id: ${id}`);
+      })
+      .catch(error => {
+        res.status(500).json({ message: error.message });
+      });
+  });
+  
+  router.put("/api/accounts/:id", (req, res) => {
+    accounts
+      .updateAccountById(req.params.id, req.body)
+      .then(updated => {
+        res.status(202).json("This account has been updated");
+      })
+      .catch(error => {
+        res.status(500).json({ message: error.message });
+      });
+  });
+  
+  router.delete("/api/accounts/:id", (req, res) => {
+    accounts
+      .deleteAccountById(req.params.id)
+      .then(deleted => {
+        res.status(202).json("This account has been deleted");
+      })
+      .catch(error => {
+        res.status(500).json({ message: error.message });
+      });
+  });
+
 module.exports = router;
